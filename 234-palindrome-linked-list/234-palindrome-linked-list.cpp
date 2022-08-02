511b2f8323bd3cc9fd51ec2cd7b5069bf29b78b6
Prathamesh patil <73377212+prathameshpatil2002@users.noreply.github.com>
2022-08-02 11:11:25 +0530

    Time: 328 ms (63.95%), Space: 113.9 MB (91.30%) - LeetHub

---
+++ 234-palindrome-linked-list/234-palindrome-linked-list.cpp
@@ -0,0 +1,95 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* reverse(ListNode* head) {
+        if(head==NULL){
+            return head;
+        }
+        ListNode*curr = head->next;
+        ListNode*pre = head;
+        ListNode*tmp = NULL;
+        head->next = NULL;
+        while(curr != NULL){
+            tmp = curr->next;
+            curr->next = pre;
+            pre = curr;
+            curr = tmp;
+        }
+        return pre;
+    }
+    bool isPalindrome(ListNode* head) {
+        if(head==NULL || head->next ==NULL){
+            return true;
+        }
+        int l = 0;
+        ListNode*n = head;
+        while(n){
+            l++;
+            n =n->next;
+        }
+        if(l%2==0){
+            int c = 1;
+            ListNode*t = head;
+            while(c<(l/2)){
+                c++;
+                t = t->next;
+            }
+            ListNode*l2 = t->next;
+            t->next = NULL;
+            ListNode*l1 = head;
+            l2 = reverse(l2);
+            int flag = 0;
+            while(l1&&l2){
+                if(l1->val!=l2->val){
+                    flag =1;
+                    break;
+                }
+                l1 = l1->next;
+                l2 = l2->next;
+            }
+            if(flag){
+                return false;
+            }
+            else{
+                return true;
+            }
+        }
+        else if(l%2!=0){
+            int c = 1;
+            ListNode*t = head;
+            while(c<(l/2)){
+                c++;
+                t = t->next;
+            }
+            ListNode*l2 = t->next->next;
+            t->next = NULL;
+            ListNode*l1 = head;
+            l2 = reverse(l2);
+            int flag = 0;
+            while(l1&&l2){
+                if(l1->val!=l2->val){
+                    flag =1;
+                    break;
+                }
+                l1 = l1->next;
+                l2 = l2->next;
+            }
+            if(flag){
+                return false;
+            }
+            else{
+                return true;
+            }
+        }
+      return false;  
+    }
+};
