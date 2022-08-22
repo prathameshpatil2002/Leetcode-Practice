3c8d5df45605c32189789b808477e9d47ae6e39f
Prathamesh patil <73377212+prathameshpatil2002@users.noreply.github.com>
2022-08-22 23:46:40 +0530

    Time: 37 ms (49.34%), Space: 11.4 MB (31.54%) - LeetHub

---
+++ 590-n-ary-tree-postorder-traversal/590-n-ary-tree-postorder-traversal.cpp
@@ -0,0 +1,37 @@
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    vector<Node*> children;
+
+    Node() {}
+
+    Node(int _val) {
+        val = _val;
+    }
+
+    Node(int _val, vector<Node*> _children) {
+        val = _val;
+        children = _children;
+    }
+};
+*/
+
+class Solution {
+public:
+    vector<int>ret;
+    void post(Node* root){
+        if(root==NULL){
+            return;
+        }
+        for(auto it : root->children){
+            post(it);
+        }
+        ret.push_back(root->val);
+    }
+    vector<int> postorder(Node* root) {
+        post(root);
+        return ret;
+    }
+};
