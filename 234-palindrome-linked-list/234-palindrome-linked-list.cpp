e608fde192c7cce2c26a38ded4b25590e8b33b4b
Prathamesh patil <73377212+prathameshpatil2002@users.noreply.github.com>
2022-08-23 23:02:30 +0530

    Time: 490 ms (15.92%), Space: 120.7 MB (39.46%) - LeetHub

--- 234-palindrome-linked-list/234-palindrome-linked-list.cpp
+++ 234-palindrome-linked-list/234-palindrome-linked-list.cpp
@@ -27,69 +27,102 @@ public:
         return pre;
     }
     bool isPalindrome(ListNode* head) {
-        if(head==NULL || head->next ==NULL){
-            return true;
-        }
+      //   if(head==NULL || head->next ==NULL){
+      //       return true;
+      //   }
         int l = 0;
         ListNode*n = head;
         while(n){
             l++;
             n =n->next;
         }
-        if(l%2==0){
-            int c = 1;
-            ListNode*t = head;
-            while(c<(l/2)){
-                c++;
-                t = t->next;
-            }
-            ListNode*l2 = t->next;
-            t->next = NULL;
-            ListNode*l1 = head;
-            l2 = reverse(l2);
-            int flag = 0;
-            while(l1&&l2){
-                if(l1->val!=l2->val){
-                    flag =1;
-                    break;
-                }
-                l1 = l1->next;
-                l2 = l2->next;
-            }
-            if(flag){
-                return false;
+      //   if(l%2==0){
+      //       int c = 1;
+      //       ListNode*t = head;
+      //       while(c<(l/2)){
+      //           c++;
+      //           t = t->next;
+      //       }
+      //       ListNode*l2 = t->next;
+      //       t->next = NULL;
+      //       ListNode*l1 = head;
+      //       l2 = reverse(l2);
+      //       int flag = 0;
+      //       while(l1&&l2){
+      //           if(l1->val!=l2->val){
+      //               flag =1;
+      //               break;
+      //           }
+      //           l1 = l1->next;
+      //           l2 = l2->next;
+      //       }
+      //       if(flag){
+      //           return false;
+      //       }
+      //       else{
+      //           return true;
+      //       }
+      //   }
+      //   else if(l%2!=0){
+      //       int c = 1;
+      //       ListNode*t = head;
+      //       while(c<(l/2)){
+      //           c++;
+      //           t = t->next;
+      //       }
+      //       ListNode*l2 = t->next->next;
+      //       t->next = NULL;
+      //       ListNode*l1 = head;
+      //       l2 = reverse(l2);
+      //       int flag = 0;
+      //       while(l1&&l2){
+      //           if(l1->val!=l2->val){
+      //               flag =1;
+      //               break;
+      //           }
+      //           l1 = l1->next;
+      //           l2 = l2->next;
+      //       }
+      //       if(flag){
+      //           return false;
+      //       }
+      //       else{
+      //           return true;
+      //       }
+      //   }
+      // return false;  
+        
+      /* without reversing */
+        
+     stack<int> s;
+     ListNode*slow = head,*fast = head;
+        
+    while(slow&&fast&&fast->next){
+        s.push(slow->val);
+        slow = slow->next;
+        fast = fast->next->next;
+    }
+        //s.pop();
+        if(l%2){
+            slow = slow->next;
+        }
+         int flag = 0;
+        
+        while(slow){
+            if(s.top()==slow->val){
+                s.pop();
+                slow = slow->next;
+                continue;
             }
             else{
-                return true;
+                flag = 1;
+                break;
             }
+            
         }
-        else if(l%2!=0){
-            int c = 1;
-            ListNode*t = head;
-            while(c<(l/2)){
-                c++;
-                t = t->next;
-            }
-            ListNode*l2 = t->next->next;
-            t->next = NULL;
-            ListNode*l1 = head;
-            l2 = reverse(l2);
-            int flag = 0;
-            while(l1&&l2){
-                if(l1->val!=l2->val){
-                    flag =1;
-                    break;
-                }
-                l1 = l1->next;
-                l2 = l2->next;
-            }
-            if(flag){
-                return false;
-            }
-            else{
-                return true;
-            }
+        if(!flag){
+            return true;
         }
-      return false;  
+        return false;
     }
 };
