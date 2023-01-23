940b238e1bfb58b8694d0a9ed1baa5fa9ddad16d
Prathamesh patil <73377212+prathameshpatil2002@users.noreply.github.com>
2023-01-23 15:09:47 +0530

    Time: 344 ms (29.83%), Space: 74.2 MB (9.27%) - LeetHub

---
+++ 0997-find-the-town-judge/0997-find-the-town-judge.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int findJudge(int n, vector<vector<int>>& trust) {
+        int tj = -1;
+        
+        vector<int>g[n+1];
+        map<int,int>m;
+        for(auto it : trust){
+            g[it[1]].push_back(it[0]);
+            m[it[0]]++;
+        }
+        
+        int su = n*(n+1);
+        su/=2;
+        
+        for(int i=1; i<=n; i++){
+            if(g[i].size()==n-1&&m[i]==0){
+                return i;
+            }
+        }
+        return tj;
+    }
+};
