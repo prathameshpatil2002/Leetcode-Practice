56230470064fa2ef3d455494f8cd6160867aa917
Prathamesh patil <73377212+prathameshpatil2002@users.noreply.github.com>
2023-04-14 22:51:43 +0530

    Time: 11 ms (21.08%), Space: 8.7 MB (81.61%) - LeetHub

--- 0120-triangle/0120-triangle.cpp
+++ 0120-triangle/0120-triangle.cpp
@@ -2,19 +2,37 @@ class Solution {
 public:
 	int minimumTotal(vector<vector<int>>& triangle) {
         int n = triangle.size();
-        vector<vector<int>>dp(n,vector<int>(n));
+//        vector<vector<int>>dp(n,vector<int>(n));
         
-        for(int i=n-1; i>=0; i--){
-            for(int j=i; j>=0; j--){
-                if(i==n-1){
-                    dp[i][j] = triangle[i][j];
-                }
-                else{
-                    dp[i][j] = min(dp[i+1][j],dp[i+1][j+1])+triangle[i][j];
-                }
-            }
+//         for(int i=n-1; i>=0; i--){
+//             for(int j=i; j>=0; j--){
+//                 if(i==n-1){
+//                     dp[i][j] = triangle[i][j];
+//                 }
+//                 else{
+//                     dp[i][j] = min(dp[i+1][j],dp[i+1][j+1])+triangle[i][j];
+//                 }
+//             }
+//         }
+        // space optimized;
+        
+    vector<int> front(n,0), cur(n,0);
+    
+    for(int j=0;j<n;j++){
+        front[j] = triangle[n-1][j];
+    }
+    
+    for(int i=n-2; i>=0; i--){
+        for(int j=i; j>=0; j--){
+            
+            int down = triangle[i][j]+front[j];
+            int diagonal = triangle[i][j]+front[j+1];
+            
+            cur[j] = min(down, diagonal);
         }
-        
-        return dp[0][0];
+        front=cur;
+    }
+    
+    return front[0];
 	}
 };
