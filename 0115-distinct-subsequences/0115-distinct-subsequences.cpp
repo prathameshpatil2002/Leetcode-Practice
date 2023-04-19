b1b2d1083f86ec5dd21b9f763dde7a139c9ccc4b
Prathamesh patil <73377212+prathameshpatil2002@users.noreply.github.com>
2023-04-19 15:49:24 +0530

    Time: 17 ms (96.89%), Space: 10.7 MB (81.45%) - LeetHub

---
+++ 0115-distinct-subsequences/0115-distinct-subsequences.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int numDistinct(string s, string t) {
+        int n  = s.size();
+        int m  = t.size();
+        
+        
+        unsigned dp[1001][1001];
+        
+        for(int i=0; i<=n; i++){
+            dp[i][0] = 1;
+        }
+        
+        for(int i=1; i<=n; i++){
+            for(int j=1; j<=m; j++){
+                if(s[i-1] == t[j-1]){
+                    dp[i][j] = dp[i-1][j] + dp[i-1][j-1];
+                }
+                else{
+                    dp[i][j] = dp[i-1][j];
+                }
+            }
+        }
+        
+        return int(dp[n][m]);
+    }
+};
