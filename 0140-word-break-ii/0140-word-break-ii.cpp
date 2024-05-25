5b57aa67bfefedfe4def62d625c74058b5b32393
Prathamesh patil <73377212+prathameshpatil2002@users.noreply.github.com>
2024-05-25 20:59:27 +0530

    Time: 0 ms (100.00%), Space: 8.8 MB (87.09%) - LeetHub

---
+++ 0140-word-break-ii/0140-word-break-ii.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    void backtrack(const string& s, const unordered_set<string>& wordSet,
+                   string& currentSentence, vector<string>& results,
+                   int startIndex) {
+       
+        if (startIndex == s.length()) {
+            results.push_back(currentSentence);
+            return;
+        }
+
+       
+        for (int endIndex = startIndex + 1; endIndex <= s.length();
+             ++endIndex) {
+            string word = s.substr(startIndex, endIndex - startIndex);
+           
+            if (wordSet.find(word) != wordSet.end()) {
+                string originalSentence = currentSentence;
+                if (!currentSentence.empty()) currentSentence += " ";
+                currentSentence += word;
+               
+                backtrack(s, wordSet, currentSentence, results, endIndex);
+                
+                currentSentence = originalSentence;
+            }
+        }
+    }
+    vector<string> wordBreak(string s, vector<string>& wordDict) {
+        
+        unordered_set<string> wordSet(wordDict.begin(), wordDict.end());
+        vector<string> results;
+        string currentSentence;
+        
+        backtrack(s, wordSet, currentSentence, results, 0);
+        return results;
+    }
+};
